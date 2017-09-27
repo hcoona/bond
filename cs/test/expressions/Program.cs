3b3eccae81ef6c5334768c77acdbc7ac1c36b7d1
Eduardo Salinas <edus@microsoft.com>
2017-09-27 14:23:50 -0700

    test change

--- cs/test/expressions/Program.cs
+++ cs/test/expressions/Program.cs
@@ -123,6 +123,7 @@
 
         static void Main(string[] args)
         {
+            /*
             Write("TranscodeCBCB.expressions",
                 new Transcoder<CompactBinaryReader<InputStream>, CompactBinaryWriter<OutputStream>>());
 
@@ -132,9 +133,11 @@
             Write("TranscodeCBSP.expressions",
                 new Transcoder<CompactBinaryReader<InputStream>, SimpleBinaryWriter<OutputStream>>(typeof(Example)));
 
+                */
             Write("DeserializeCB.expressions",
-                new DeserializerDebugView<CompactBinaryReader<InputStream>>(typeof(Example)));
+                new DeserializerDebugView<CompactBinaryReader<InputStream>>(typeof(HourlyElementAdUsage)));
 
+            /*
             Write("DeserializeSP.expressions",
                 new DeserializerDebugView<SimpleBinaryReader<InputStream>>(typeof(Example)));
 
@@ -146,12 +149,108 @@
 
             Write("SerializeSP.expressions",
                 new SerializerDebugView<SimpleBinaryWriter<OutputStream>>(typeof(Example)));
+                */
 
             Write("SerializeCB.expressions",
-                new SerializerDebugView<CompactBinaryWriter<OutputStream>>(typeof(Example)));
+                new SerializerDebugView<CompactBinaryWriter<OutputStream>>(typeof(HourlyElementAdUsage)));
+            Write("SerializeCB_with.expressions",
+                new SerializerDebugView<CompactBinaryWriter<OutputStream>>(typeof(StructWithBlobs)));
 
+            /*
             Write("SerializeXml.expressions",
                 new SerializerDebugView<SimpleXmlWriter>(typeof(Example)));
+                */
+
+            var temp = new HourlyElementAdUsage();
+            temp.TOTALAMOUNT = new decimal(23.23);
+
+            var outputStream = new OutputBuffer();
+            var writer = new CompactBinaryWriter<OutputBuffer>(outputStream);
+            var serializer = new Serializer<CompactBinaryWriter<OutputBuffer>>(typeof(HourlyElementAdUsage));
+
+            serializer.Serialize(temp, writer);
+
+            HourlyElementAdUsage item = new HourlyElementAdUsage();
+
+            var inputStream = new InputBuffer(outputStream.Data);
+            var reader = new CompactBinaryReader<InputBuffer>(inputStream);
+            var deserializer = new Deserializer<CompactBinaryReader<InputBuffer>>(typeof(HourlyElementAdUsage));
+
+            item = deserializer.Deserialize<HourlyElementAdUsage>(reader);
+
+
+            Console.ReadKey();
+
         }
     }
+
+    [global::Bond.Schema]
+    [System.CodeDom.Compiler.GeneratedCode("gbc", "0.10.0.0")]
+    public partial class HourlyElementAdUsage
+    {
+        [global::Bond.Id(16), global::Bond.Type(typeof(global::Bond.Tag.blob))]
+        public System.Decimal TOTALAMOUNT { get; set; }
+
+        public HourlyElementAdUsage()
+            : this("FastBITest.Entity.HourlyElementAdUsage", "HourlyElementAdUsage")
+        { }
+
+        protected HourlyElementAdUsage(string fullName, string name)
+        {
+            TOTALAMOUNT = new System.Decimal();
+        }
+    }
+
+    [global::Bond.Schema]
+    [System.CodeDom.Compiler.GeneratedCode("gbc", "0.10.0.0")]
+    public partial class StructWithBlobs
+    {
+        [global::Bond.Id(0)]
+        public System.ArraySegment<byte> b { get; set; }
+
+        [global::Bond.Id(1)]
+        public List<System.ArraySegment<byte>> lb { get; set; }
+
+        [global::Bond.Id(2), global::Bond.Type(typeof(global::Bond.Tag.nullable<System.ArraySegment<byte>>))]
+        public System.ArraySegment<byte> nb { get; set; }
+
+        public StructWithBlobs()
+            : this("UnitTest.StructWithBlobs", "StructWithBlobs")
+        {}
+
+        protected StructWithBlobs(string fullName, string name)
+        {
+            b = new System.ArraySegment<byte>();
+            lb = new List<System.ArraySegment<byte>>();
+        }
+    }
+    public static class BondTypeAliasConverter
+    {
+        public static decimal Convert(ArraySegment<byte> value, decimal unused)
+        {
+            var bits = new int[value.Count / sizeof(int)];
+            Buffer.BlockCopy(value.Array, value.Offset, bits, 0, bits.Length * sizeof(int));
+            return new decimal(bits);
+        }
+
+        public static ArraySegment<byte> Convert(decimal value, ArraySegment<byte> unused)
+        {
+            var bits = decimal.GetBits(value);
+            var data = new byte[bits.Length * sizeof(int)];
+            Buffer.BlockCopy(bits, 0, data, 0, data.Length);
+            return new ArraySegment<byte>(data);
+        }
+
+        public static long Convert(DateTime value, long unused)
+        {
+            return value.Ticks;
+        }
+
+        public static DateTime Convert(long value, DateTime unused)
+        {
+            return new DateTime(value);
+        }
+    }
+
+
 }
